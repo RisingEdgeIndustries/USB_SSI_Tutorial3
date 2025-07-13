20731ae1f59b977619da7f01342c49583e5c3420
rob <robert.f.valentine@gmail.com>
2025-07-12 21:50:18 -0500

    updated header comments

--- tutorial3.py
+++ tutorial3.py
@@ -1,36 +1,46 @@
-#
+# ################################################################
 # Project Name:
-# USB Bridge - Tutorial 2
+# USB Bridge - Tutorial 3
 #
 # Author: rvalentine
 # Date: 10/19/2023
 #
+#
 # Project Description:
-# ----------------------
-# Tutorial 3 performs basic read/write operations
-# with interface 1 - HID.
+# ----------------------------------------------------------------
+# Tutorial 3 shows data transfer over both BULK and INT interfaces.
+# INT frames are mixed into the BULK datastream which show users
+# can interleave data from different interfaces. In this example,
+# one could use the INT interface (from the embedded system SSI
+# side) to tell the software application how much BULK data to 
+# read from the embedded system in a single burst. From this
+# example, many other styles of implementations could be used.
+#  
 #
+# Notes:
+# ----------------------------------------------------------------
+# For increased performance and functionality from the software
+# side, develop applications using core lib (libusb) ASYNC
+# capability.
+# https://libusb.sourceforge.io/api-1.0/libusb_mtasync.html
+#
+#
+# ----------------------------------------------------------------
 # Disclaimer:
-# ----------------------
-# This code is provided as is and is not supported
-# by RisingEdgeIndsutreis in any way. The user
-# accepts all risk when running all or some of 
-# this python module.
+# ----------------------------------------------------------------
+# This library is provided strictly as example code. There is no
+# expected reliablity of operation from RisingEdgeIndustries and 
+# this source code is not to be sold or represented as a 3'd party
+# solution for commercial use. The below code is development code
+# for example use only supporting customers as they test the bridge
+# products from RisingEdgeIndustries. Nothing in this file is allowed
+# to be modified or sold in any way. No code below is released with 
+# the intention or expectation of reliable operation.
 #
-# To Do:
-# Launch thread to receive async INT packets and 
-# use main application for time.sleep delays and
-# to receive multiple blocks of BULK data.
-#
-# Set SCLK to 8Mhz and make sure fw in emulator
-# uses flow control.
-#
-# Might want to write data to file and verify it
-# as well. Nice thing to show. Justuse CSV files.
-#
-# 1. Code thread class to rx INT data
-#
-#
+# Packing this module with any 3d part code can only be done with 
+# the inclusion of this disclaimer and no modifications.
+# ################################################################
+
 
 try:
 	import Queue as queue
@@ -43,6 +53,7 @@ import time
 from USB_SSI_Libs.rei_usb_lib import USB20F_Device
 
 
+
 #
 # GLOBALs
 #
@@ -50,86 +61,104 @@ log_file_name = "tutorial3_log_file"
 
 
 
-# ----------------------------------------
-# INT receiver thread
-# ----------------------------------------
+# ----------------------------------------------------------------
+# Telemetry reciever thread
+#
+# Description:
+# Perform all USB bridge data reads in thread. This emulates
+# real world implementations where the main application loop
+# doesn't directly manage USB object accesses.
+# ----------------------------------------------------------------
 class tly_receiver(threading.Thread):
 	#
 	# Setup Init()
 	#
-	def __init__(self):
+	def __init__(self, usb_dev_handle):
 		threading.Thread.__init__(self)
 		self.isAlive = threading.Event()
 		self.isAlive.set()
 		self.Queue = queue.Queue()
+		self.usb_dev_handle = usb_dev_handle
 
 
-
-	#
-	# run method
-	#
+	# over-ride run method
+	# poll each interface until threading event set to false
 	def run(self):
-		pass
+		bulk_counter = 0
+
+		# send test case command 21d
+		data = [0]*64
+		data[0] = 21
+		self.usb_dev_handle.write_int1(data)
+
+		while(self.isAlive.isSet()):
+			ret = self.usb_dev_handle.read_int1(timeout=1)
+			if(ret[0] == 0):
+				print(f"INT Packet Received: {len(ret[1])}")
+			else:
+				if(ret[1] == -7):
+					pass
+				else:
+					print(f"INT error: {ret}")
+
+			ret = self.usb_dev_handle.rec_bulk(ep_size=64*128, timeout=1)
+			if(ret[0] == 0):
+				print(f"BULK: Received 128 packets!")
+			else:
+				if(ret[1] == -7):
+					pass
+				else:
+					print(f"BULK error: {ret}")
+
+		print(f"Exiting (isAlive value): {self.isAlive.isSet()}")
 
 
-
-
-# ----------------------------------------
+# ----------------------------------------------------------------
 # Main class
-# ----------------------------------------
+#
+# Description:
+# Main tutorial class encapsulating tutorial functionality
+#
+# ----------------------------------------------------------------
 class main:
 	def __init__(self):
 		pass
 
-
-
-	"""
-	Name: 
-	testcase1_exe()
-
-	Parameters:
-	dev_handle - usb device handle
-
-	Return:
-	Tuple consisting of error flag and the
-	associated error code if any exists.
-
-	Notes:
-	Start thread, command async INT packet generation EN, mix in 
-	1k or so BULK xfers as well and then close everything out. Store
-	in CSV file and check value, probably counter inc values as an
-	example.
-
-	"""
-	def testcase1_exe(dev_handle):
+	# testcase_exe - main test case method 
+	def testcase_exe(dev_handle):
 		# -------------------------------------------------------------------
 		print('\n\n\n===================================')
 		print('[tutorial #3 - threaded receiver]')
+		j = 0
 
 
-		#
-		# Run module
-		#
-		# open USB lib
+		# open USB device and create object
 		usb_dev0 = USB20F_Device(quiet=True, name=log_file_name)
 		usb_dev0.open_usb()
+
+		# start receiver thread
+		thd = tly_receiver(usb_dev0)
+		thd.daemon = True
+		thd.start()
 	
-		
-		# test - error with logger or something failing
-		# original error due to not calling close() method
-		usb_dev0.dump_regspace()	
+
+		time.sleep(3)
+		thd.isAlive.clear()
+
+		# Ensure thread closed
+		thd.join()
 	
 	
-		# need this to exit gracefully
+		# Close USB link gracefully
 		usb_dev0.close_usb()
 	
-	
+
 		return (0, 0)
 
 
 
 #
 # Main Application
-#
+# Run
 app = main()
-app.testcase1_exe()
+app.testcase_exe()
